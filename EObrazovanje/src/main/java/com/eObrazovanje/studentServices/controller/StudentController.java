938261d0f97f6b29aed7c8000640653a92ed97ba
Valentina Mackovic <mackovicvalentina@gmail.com>
2020-06-29 15:49:15 +0200

    CORS

--- EObrazovanje/src/main/java/com/eObrazovanje/studentServices/controller/StudentController.java
+++ EObrazovanje/src/main/java/com/eObrazovanje/studentServices/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.eObrazovanje.studentServices.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.eObrazovanje.studentServices.service.StudentServiceInterface;
 
 @RestController
 @RequestMapping("api/students")
+@CrossOrigin(value="*")
 public class StudentController {
 
 	@Autowired
